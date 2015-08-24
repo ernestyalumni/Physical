ec0066357143b1c3e425d4e128bb401576d7f9dc
E Yeung <ernestyalumni@gmail.com>
2015-08-24 13:30:37 -0700

    Update query.py
    “This product uses data provided by the National Institute of Standards and Technology (NIST) but is not endorsed or certified by NIST.”

--- query.py
+++ query.py
@@ -34,7 +34,8 @@
 ## twitter      : ernestyalumni                                                   
 ## youtube      : ernestyalumni                                                   
 ## wordpress    : ernestyalumni                                                    
-##                                                                                  
+## 
+## Note, “This product uses data provided by the National Institute of Standards and Technology (NIST) but is not endorsed or certified by NIST.”
 ############################################################################ 
 import decimal
 from decimal import Decimal
